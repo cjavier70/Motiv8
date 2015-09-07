7ae4824b32f329569362f9c9d569c5f477b82bc8
cjavier70 <cameron.k.javier@gmail.com>
2015-09-07 23:45:23 +0000

    Error logs and all, just got the post to work

--- motiv8.py
+++ motiv8.py
@@ -1,29 +1,52 @@
 from flask import Flask, g, render_template, url_for, request, Response, jsonify
-import _mysql
-db = _mysql.connect("motiv8instance.cwkcvq4ycfyc.us-west-2.rds.amazonaws.com","admin","motivate","innodb")
+import MySQLdb
 import sys 
-sys.stdout=open('/var/www/motiv8/log/output.log', 'w')
+import atexit
+db = MySQLdb.connect("motiv8instance.cwkcvq4ycfyc.us-west-2.rds.amazonaws.com","admin","motivate","innodb")
+sys.stdout=open('/var/www/motiv8/log/output.log', 'a+w')
 
 app = Flask(__name__)
 
 
-@app.route('/user', methods=['GET','POST'])
+@app.route('/')
 def index():
+    return "Welcome to Motiv8 API"
+
+@app.route('/user', methods=['GET','POST'])
+def user():
     if request.method=='GET':
         return "Welcome to Motiv8 API"
     else:
-        try:
-            firstName = request.get_json().get('firstName', '')
-            lastName = request.get_json().get('lastName', '')
-            accessToken = request.get_json().get('accessToken', '')
-        except:
-            sys.stdout.close()
-            return 'Error: Fields are firstName, lastName, accessToken. \
-             You likely did not pass json to this URL'
+        return postUser()
 
-        #db.query("INSERT INTO User (firstName, lastName, accessToken) VALUES ('Gabrielle', 'Javier', 'fake2')")
-        return body
+def postUser():
+    r = None
+    try:
+        firstName = request.get_json().get('firstName', '')
+        lastName = request.get_json().get('lastName', '')
+        accessToken = request.get_json().get('accessToken', '')
+    except:
+        return 'Error: Fields are firstName, lastName, accessToken. \
+         If you sent those, you likely did not pass json to this URL'
+    try:
+        sql = "INSERT INTO User (firstName, lastName, accessToken) VALUES ('%s', '%s', '%s')" \
+         % (firstName, lastName, accessToken)
+        cur = db.cursor()
+        cur.execute(sql)
+        lastId = cur.lastrowid
+        db.commit()
+    except:
+        return "Insert Into User Threw an exception"
+    sys.stdout.flush()
+    print "whats problem"
+    return "%s" % lastId
+
+def clean():
+    print "Clean complete"
+    sys.stdout.flush()
+    sys.stdout.close()
+
+atexit.register(clean)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
