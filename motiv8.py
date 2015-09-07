a3280018b2bcebaed545ffca84f48887e3dca4bd
EC2 Default User <ec2-user@ip-172-31-16-24.us-west-2.compute.internal>
2015-09-07 18:40:12 +0000

    Add logs

--- motiv8.py
+++ motiv8.py
@@ -1,13 +1,22 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import _mysql
+db = _mysql.connect("motiv8instance.cwkcvq4ycfyc.us-west-2.rds.amazonaws.com","admin","motivate","innodb")
+import sys 
+sys.stdout=open('log/output.log', 'w')
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/user', methods=['GET','POST'])
 def index():
-        body = "Welcome to Motiv8 API"
+    if request.method=='GET':
+        return "Welcome to Motiv8 API"
+    else:
+        body = "Your json passed me %s" % request.get_json().get('firstName', '')
+        print "LOOK"
+        print request.get_json()
+        #db.query("INSERT INTO User (firstName, lastName, accessToken) VALUES ('Gabrielle', 'Javier', 'fake2')")
         return body
-
 
 if __name__ == '__main__':
     app.run(debug=True)
